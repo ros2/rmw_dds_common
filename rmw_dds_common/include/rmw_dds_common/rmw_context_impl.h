b913878c6a76b4187d1393af9819d10393be9500
ivanpauno <ivanpauno@ekumenlabs.com>
2019-09-13 18:17:30 -0700

    Add rmw_context_impl_t and related functions
    Signed-off-by: ivanpauno <ivanpauno@ekumenlabs.com>

---
+++ rmw_dds_common/include/rmw_dds_common/rmw_context_impl.h
@@ -0,0 +1,71 @@
+// Copyright 2019 Open Source Robotics Foundation, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#ifndef RMW_DDS_COMMON__RMW_CONTEXT_IMPL_H_
+#define RMW_DDS_COMMON__RMW_CONTEXT_IMPL_H_
+
+#include "rmw/init.h"
+#include "rmw/types.h"
+
+#ifdef __cplusplus
+extern "C"
+{
+#endif
+
+typedef struct rmw_context_impl_t
+{
+  const rmw_gid_t * gid;
+  void * topic_cache;
+  void * node_cache;
+  void * data;
+} rmw_context_impl_t;
+
+
+/// Init context implementation
+/**
+ * Init the context implementation in the passed context.
+ *
+ * \param[in] gid Gid of the DDS participant, or similar unique identifier.
+ *    Should be a valid pointer previously allocated.
+ * \param[in] data Implementation specific detail.
+ *   For DDS based implementation it may be only a Participant.
+ * \return RMW_RET_BAD_ALLOC, or
+ * \return RMW_RET_OK.
+ */
+RMW_DDS_COMMON_PUBLIC
+rmw_ret_t
+rmw_init_context_impl(
+  const rmw_gid_t * gid,
+  void * data,
+  rmw_context_t * context);
+
+/// Finish context implementation
+/**
+ * Finish the context implementation in the passed context.
+ * If gid or data were allocated dinamically before calling
+ * `rmw_init_context_impl`, they should be freed before calling
+ * this function.
+ *
+ * \return RMW_RET_BAD_ALLOC, or
+ * \return RMW_RET_OK.
+ */
+RMW_DDS_COMMON_PUBLIC
+void
+rmw_context_impl_fini(rmw_context_t * context);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif  // RMW_DDS_COMMON__RMW_CONTEXT_IMPL_H_
---
+++ rmw_dds_common/src/rmw_context_impl.cpp
@@ -0,0 +1,65 @@
+// Copyright 2019 Open Source Robotics Foundation, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include "rmw/allocators.h"
+#include "rmw/init.h"
+#include "rmw/types.h"
+#include "rmw/impl/cpp/macros.hpp"
+
+#include "rmw_dds_common/node_cache.hpp"
+#include "rmw_dds_common/topic_cache.hpp"
+#include "rmw_dds_common/rmw_context_impl.h"
+
+extern "C"
+{
+using rmw_dds_common::NodeCache;
+using rmw_dds_common::TopicCache;
+
+rmw_ret_t
+rmw_context_impl_init(
+  const rmw_gid_t * gid,
+  void * data,
+  rmw_context_t * context)
+{
+  NodeCache * node_cache = nullptr;
+  TopicCache * topic_cache = nullptr;
+  context->impl = reinterpret_cast<rmw_context_impl_t *>(rmw_allocate(sizeof(rmw_context_impl_t)));
+  if (nullptr == context->impl) {
+    goto fail;
+  }
+  RMW_TRY_PLACEMENT_NEW(node_cache, node_cache, goto fail, NodeCache, );
+  RMW_TRY_PLACEMENT_NEW(topic_cache, topic_cache, goto fail, TopicCache, );
+  context->impl->gid = gid;
+  context->impl->node_cache = node_cache;
+  context->impl->topic_cache = topic_cache;
+  context->impl->data = data;
+
+fail:
+  delete context->impl;
+  delete node_cache;
+  delete topic_cache;
+  return RMW_RET_BAD_ALLOC;
+}
+
+void
+rmw_context_impl_fini(rmw_context_t * context)
+{
+  delete reinterpret_cast<NodeCache *>(context->impl->node_cache);
+  delete reinterpret_cast<TopicCache *>(context->impl->topic_cache);
+  context->impl->gid = nullptr;
+  context->impl->node_cache = nullptr;
+  context->impl->topic_cache = nullptr;
+  context->impl->data = nullptr;
+}
+}  // extern "C"
