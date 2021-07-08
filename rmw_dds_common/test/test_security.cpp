d55fcbc14bcf9fac71549ca31804a628ef230b0d
Chris Lalancette <clalancette@openrobotics.org>
2021-07-08 09:15:24 -0400

    Add support for Certificate Revocation List files. (#52)
    At least Fast-RTPS and CycloneDDS support Certificate
    Revocation Lists, so add it as one of the possible files
    in the enclave.  Note that it is an optional file; if
    an enclave doesn't have it, then the key will be missing
    from the returned map.
    
    Signed-off-by: Chris Lalancette <clalancette@openrobotics.org>

--- rmw_dds_common/test/test_security.cpp
+++ rmw_dds_common/test/test_security.cpp
@@ -129,3 +129,49 @@ TEST(test_security, file_missing)
   ASSERT_FALSE(rmw_dds_common::get_security_files("", dir.string(), security_files));
   ASSERT_EQ(security_files.size(), 0UL);
 }
+
+TEST(test_security, optional_file_exist)
+{
+  rcpputils::fs::path dir = rcpputils::fs::path("./test_folder");
+  rcpputils::fs::remove_all(dir);
+  EXPECT_TRUE(rcpputils::fs::create_directories(dir));
+  EXPECT_TRUE(rcpputils::fs::exists(dir));
+  EXPECT_TRUE(rcpputils::fs::is_directory(dir));
+
+  std::array<std::string, 7> required_files = {
+    "identity_ca.cert.pem", "cert.pem", "key.pem",
+    "permissions_ca.cert.pem", "governance.p7s", "permissions.p7s", "crl.pem",
+  };
+  for (const std::string & filename : required_files) {
+    rcpputils::fs::path full_path = dir / filename;
+    std::ofstream output_buffer{full_path.string()};
+    output_buffer << "test";
+    ASSERT_TRUE(rcpputils::fs::exists(full_path));
+  }
+
+  std::unordered_map<std::string, std::string> security_files;
+  ASSERT_TRUE(rmw_dds_common::get_security_files("", dir.string(), security_files));
+
+  EXPECT_EQ(
+    security_files["IDENTITY_CA"],
+    rcpputils::fs::path("./test_folder/identity_ca.cert.pem").string());
+  EXPECT_EQ(
+    security_files["CERTIFICATE"],
+    rcpputils::fs::path("./test_folder/cert.pem").string());
+  EXPECT_EQ(
+    security_files["PRIVATE_KEY"],
+    rcpputils::fs::path("./test_folder/key.pem").string());
+  EXPECT_EQ(
+    security_files["PERMISSIONS_CA"],
+    rcpputils::fs::path("./test_folder/permissions_ca.cert.pem").string());
+  EXPECT_EQ(
+    security_files["GOVERNANCE"],
+    rcpputils::fs::path("./test_folder/governance.p7s").string());
+  EXPECT_EQ(
+    security_files["PERMISSIONS"],
+    rcpputils::fs::path("./test_folder/permissions.p7s").string());
+
+  EXPECT_EQ(
+    security_files["CRL"],
+    rcpputils::fs::path("./test_folder/crl.pem").string());
+}
